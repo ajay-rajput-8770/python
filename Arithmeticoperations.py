4f50db827279353c73d445f76b3d55c4dc407b18
AjayRajput-ui <rajputboy2045@gmail.com>
2026-01-01 21:51:24 +0530

    1 day

---
+++ Arithmeticoperations.py
@@ -0,0 +1,12 @@
+# Arithmetic operations
+print("Ajay Rajput")
+print("rollno 0818CL241013")
+a = 15
+b = 5
+print("The addition is ", a+b)
+print("The Subtraction is", a-b)
+print("The Multiplication is", a*b)
+print("The Division is", a/b)
+print("The Module division:", a%b)
+print("The power is", a**b)
+print("The modulus is", a//b)
---
+++ relationaloperation.py
@@ -0,0 +1,11 @@
+
+# Relational operations
+print("Ajay Rajput")
+print("rollno 0818CL241013")
+a = 10
+b = 5
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a == b:", a == b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
