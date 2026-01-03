e599f17bc342a38a2e40cc98e12df4907f29e4fd
AjayRajput-ui <rajputboy2045@gmail.com>
2026-01-03 23:07:48 +0530

    3 days

---
+++ membershipoperations.py
@@ -0,0 +1,10 @@
+# membership operations
+print("Ajay Rajput")
+print("rollno 0818CL241013")
+x = [1, 2, 3, 4, 5]
+print(3 in x)
+print(6 in x)
+print(2 not in x)
+print(7 not in x)
+print(3 not in x)
+print(6 not in x)
---
+++ oddandeven.py
@@ -0,0 +1,6 @@
+ #6 wether the number entered by the user is odd and even
+a = int(input("Enter a number "))
+if a % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
